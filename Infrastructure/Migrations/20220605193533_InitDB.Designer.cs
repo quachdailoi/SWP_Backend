149507e02946c60170eb0b1eb090a6ad2aa1c52d
quachdailoi <loiqd.work@gmail.com>
2022-06-06 03:09:59 +0700

    Complete Config automatic migration when deploying heroku

--- Infrastructure/Migrations/20220605193533_InitDB.Designer.cs
+++ Infrastructure/Migrations/20220605193533_InitDB.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SECapstoneEvaluation.Infrastructure.Data;
@@ -11,9 +12,10 @@ using SECapstoneEvaluation.Infrastructure.Data;
 namespace Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220605193533_InitDB")]
+    partial class InitDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
